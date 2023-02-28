7f5e4344fc6ff858b9b97d5dbd8e7d7c4f24319f
hu243285237 <243285237@qq.com>
2023-02-28 17:11:09 +0800

    feat: 导航栏

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import config from '../../../package.json';
 import logo from '../../logo.svg';
 import './index.scss';
 
@@ -6,12 +7,28 @@ export default function () {
     <header className="header-container">
       <h1 className="title">PICTURE CUTTER</h1>
       <p className="description">一个将长图片自动分段切割的工具</p>
-      {/* <div className="nav">
-        <label className="nav-item">案例教程</label>
-        <label className="nav-item">工程源码</label>
-        <label className="nav-item">开发日志</label>
-        <label className="nav-item">版本v1.0.0</label>
-      </div> */}
+      <div className="nav">
+        {/* <label className="nav-item">案例教程</label> */}
+        <label
+          className="nav-item"
+          onClick={() => {
+            window.open('https://github.com/hu243285237/PictureCutter_Web');
+          }}
+        >
+          工程源码
+        </label>
+        <label
+          className="nav-item"
+          onClick={() => {
+            window.open(
+              'https://github.com/hu243285237/PictureCutter_Web/blob/master/README.md'
+            );
+          }}
+        >
+          文档说明
+        </label>
+        <label className="nav-item">版本v{config.version}</label>
+      </div>
       <img className="logo" src={logo} alt="logo" draggable="false" />
     </header>
   );
