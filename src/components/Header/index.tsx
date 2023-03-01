b835c2111059782f1755e5ae2bc09ebdaf8542b1
hu243285237 <243285237@qq.com>
2023-03-01 15:40:20 +0800

    feat: logo animation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,11 @@
+import { useState } from 'react';
 import config from '../../../package.json';
 import logo from '../../logo.svg';
 import './index.scss';
 
 export default function () {
+  const [isResLoaded, setIsResLoaded] = useState<boolean>(false);
+
   return (
     <header className="header-container">
       <h1 className="title">PICTURE CUTTER</h1>
@@ -38,7 +41,17 @@ export default function () {
           版本v{config.version}
         </label>
       </div>
-      <img className="logo" src={logo} alt="logo" draggable="false" />
+      <div className={['logo', isResLoaded && 'logo-show'].join(' ')}>
+        <img
+          className="logo-img"
+          src={logo}
+          alt="logo"
+          draggable="false"
+          onLoad={() => {
+            setIsResLoaded(true);
+          }}
+        />
+      </div>
     </header>
   );
 }
